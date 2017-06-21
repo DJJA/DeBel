a1f7588dd4feae6e024a0230071121767aa5fd72
Dirk-Jan de Beijer <djjadebeijer@gmail.com>
2017-06-21 14:16:25 +0200

    added subclasses for Log

---
+++ DeBel/Desktop/De Bel/LogDateLabel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace De_Bel
+{
+    public class LogDateLabel : Log
+    {
+        public LogDateLabel(int doorbellId, int userId, DateTime dateTime, string picturePath, string errorMessage) : base(doorbellId, userId, dateTime, picturePath, errorMessage)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "--==   " + DateTime.ToString("dd-MM-yyyy") + "   ==--";
+        }
+    }
+}
